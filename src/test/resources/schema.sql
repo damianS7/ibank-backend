60e515b24402d03eaca0c5c2d8febda9d22d28f7
damianS7 <josepwnz@gmail.com>
2021-12-03 20:07:02 +0100

    Tabla por defecto de los usuarios

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.users (
-    id integer NOT NULL AUTO_INCREMENT,
+    id integer AUTO_INCREMENT primary key,
     username character varying(30) UNIQUE NOT NULL,
-    password character varying NOT NULL,
     email character varying(255) UNIQUE NOT NULL,
+    password character varying NOT NULL,
     role character varying(30)
 );
