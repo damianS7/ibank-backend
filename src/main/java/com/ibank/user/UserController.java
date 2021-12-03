9e43379b4d74aa4912a97fda62bf0f2c7c093123
damianS7 <josepwnz@gmail.com>
2021-12-03 20:11:15 +0100

    El endpoint de registro ahora es signup. Ahora se devuelven DTO en lugar de Entity directamente. Validacion de los datos recibidos.

--- src/main/java/com/ibank/user/UserController.java
+++ src/main/java/com/ibank/user/UserController.java
@@ -1,12 +1,10 @@
 package com.ibank.user;
 
-import com.ibank.user.exception.EmailTakenException;
-import com.ibank.user.exception.UsernameTakenException;
-import com.ibank.user.http.RegistrationResponse;
-import com.ibank.user.http.UserUpdateRequest;
-import com.ibank.user.http.UserUpdateResponse;
+import com.ibank.user.http.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -18,30 +16,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Endpoint para actualizar los datos de usuario
-
     /**
      * Endpoint para la actualizacion de usuarios existentes.
      *
-     * @param request
+     * @param updateRequest La peticion con los datos del usuario a actualizar
      * @return Creado el usuario devuelve null
      */
     @PutMapping(path = "/api/v1/users", consumes = "application/json")
-    public User update(@RequestBody UserUpdateRequest request) {
-        return userService.update(request);
+    public UserUpdateResponse updateUser(@Valid @RequestBody UserUpdateRequest updateRequest) {
+        return new UserUpdateResponse(userService.updateUser(updateRequest));
     }
-
-    // Este metodo no deberia tirar exceptions ??? ...
 
     /**
      * Endpoint para la creacion de nuevos usuarios.
      *
-     * @param user
+     * @param signupRequest Los datos del usuario que se va a registrar
      * @return Creado el usuario devuelve null
      */
-    @PostMapping(path = "/api/v1/users/register", consumes = "application/json")
-    public User create(@RequestBody User user) {
-        return userService.register(user);
+    @PostMapping(path = "/api/v1/users/signup", consumes = "application/json")
+    public UserSignupResponse createUser(@Valid @RequestBody UserSignupRequest signupRequest) {
+        return new UserSignupResponse(userService.registerUser(signupRequest));
     }
-
 }
