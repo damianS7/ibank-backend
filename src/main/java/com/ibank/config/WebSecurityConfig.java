7acacefc49f0874ec235f9b0454be8c89af757ab
damianS7 <josepwnz@gmail.com>
2021-12-03 20:08:21 +0100

    Renombrado el endpoint a signup

--- src/main/java/com/ibank/config/WebSecurityConfig.java
+++ src/main/java/com/ibank/config/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and().cors()
             .and().csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/v1/users/login/**", "/api/v1/users/register/**")
+            .antMatchers("/api/v1/users/login/**", "/api/v1/users/signup/**")
             .permitAll()
             .anyRequest().authenticated()
             .and().addFilter(new AuthorizationFilter(authenticationManager()));
