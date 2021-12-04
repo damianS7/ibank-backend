053da679d0afbb35ad2985fad681038f757b6866
damianS7 <josepwnz@gmail.com>
2021-12-04 16:34:30 +0100

    SpringBoot devtools

--- src/main/java/com/ibank/IBankApplication.java
+++ src/main/java/com/ibank/IBankApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IBankApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.main.lazy-initialization", "false");
         SpringApplication.run(IBankApplication.class, args);
     }
-
 }
