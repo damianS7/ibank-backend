3752c48816eecac5c7f027a2e6728fd3edb455fe
damianS7 <josepwnz@gmail.com>
2021-12-07 18:39:04 +0100

    Ahora el token JWT contiene los datos del usuario

--- src/main/java/com/ibank/utils/JwtUtil.java
+++ src/main/java/com/ibank/utils/JwtUtil.java
@@ -1,45 +1,68 @@
 package com.ibank.utils;
 
 import com.ibank.config.SecurityConstants;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.ibank.user.User;
+import io.jsonwebtoken.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class JwtUtil {
 
     /**
-     * Crea un token unico para el nombre de usuario indicado
-     * @param username Nombre del usuario que va a ser cifrado en el token
-     * @return El token generado con los datos de usuario cifrados en el token
+     * Crea un token unico para el usuario indicado
+     *
+     * @param user Usuario sobre el que se genera el token
+     * @return El token generado con los datos de usuario
      */
-    public static String generateToken (String username) {
-        return Jwts.builder()
-                .setIssuer(SecurityConstants.ISSUER_INFO)
-                .setIssuedAt(new Date())
-                .setSubject(username)
-                // Puede ser de utilidad mas adelante
-                // .setId(userId)
-                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1)))
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET_KEY).compact();
+    public static String createToken(User user) {
+        return JwtUtil.createTokenFromUserDetails(user.getId(), user.getUsername(), user.getPassword(), user.getEmail());
     }
 
     /**
-     * Se procesa el token y se recupera el usuario.
-     * @param token Contiene el token de seguridad que se envia con la peticion
-     * @return El nombre de usuario cifrado en el token
-     * @throws ExpiredJwtException El token de seguridad ha expirado
+     * Crea un token unico para el usuario indicado
+     *
+     * @return El token generado con los datos de usuario
+     */
+    public static String createTokenFromUserDetails(Long id, String username, String password, String email) {
+        Claims claims = Jwts.claims();
+        claims.put("id", id);
+        claims.put("username", username);
+        claims.put("password", password);
+        claims.put("email", email);
+
+        return Jwts.builder()
+            .setIssuer(SecurityConstants.ISSUER_INFO)
+            .setIssuedAt(new Date())
+            .setClaims(claims)
+            .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1)))
+            //.setExpiration(new Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(1)))
+            .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET_KEY.getBytes(StandardCharsets.UTF_8))
+            .compact();
+    }
+
+    /**
+     * Convierte un token valido en una instancia de User
+     *
+     * @param token - El token
+     * @return User - Una instancia de User que contiene los datos del usuario contenidos en el token
+     * @throws ExpiredJwtException   El token de seguridad ha expirado
      * @throws MalformedJwtException El formato del token no es correcto
      */
-    public static String extractUsernameFromToken (String token) throws MalformedJwtException, ExpiredJwtException {
-        return Jwts.parser()
-                .setSigningKey(SecurityConstants.SECRET_KEY)
-                .parseClaimsJws(token.replace(SecurityConstants.TOKEN_BEARER_PREFIX, ""))
-                .getBody()
-                .getSubject();
+    public static User parseToken(String token)
+        throws MalformedJwtException, ExpiredJwtException, SignatureException {
+        Claims claims = Jwts.parser()
+            .setSigningKey(SecurityConstants.SECRET_KEY.getBytes(StandardCharsets.UTF_8))
+            .parseClaimsJws(token.replace(SecurityConstants.TOKEN_BEARER_PREFIX, ""))
+            .getBody();
+
+        return new User(
+            Long.parseLong(claims.get("id").toString()),
+            claims.get("username").toString(),
+            claims.get("email").toString(),
+            claims.get("password").toString()
+        );
     }
 }
 
