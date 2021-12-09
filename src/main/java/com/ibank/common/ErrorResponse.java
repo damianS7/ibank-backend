9014f11333f4d0126a397c85ea5ff6576c17ec4a
damianS7 <josepwnz@gmail.com>
2021-12-09 22:14:18 +0100

    Utilizado para las respuestas con error producidas en cualquier llamada a la api

---
+++ src/main/java/com/ibank/common/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.ibank.common;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    public String errorMessage;
+}
