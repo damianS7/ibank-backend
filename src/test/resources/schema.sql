983d274c715e3295f65c75afa1094409fdbdf5c6
damianS7 <josepwnz@gmail.com>
2021-12-20 18:09:50 +0100

    Se elimina schema.sql ya que se va a usar JPA

--- src/test/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.users (
-    id integer AUTO_INCREMENT primary key,
-    username character varying(30) UNIQUE NOT NULL,
-    email character varying(255) UNIQUE NOT NULL,
-    password character varying NOT NULL,
-    role character varying(30)
-);
